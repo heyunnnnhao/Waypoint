7cbbeb545eade29c68558c70535a88e692e84956
heyunnnnhao <heyunhaotq@outlook.com>
2022-02-25 18:55:26 +0800

    update

--- src/Waypoint.tsx
+++ src/Waypoint.tsx
@@ -2,17 +2,20 @@ import React, { useState, useEffect, useRef } from 'react';
 import './style/Waypoint.scss';
 
 export default function Waypoint({ children }): JSX.Element {
+  const [exposed, setExposed] = useState(false);
   const waypointRef = useRef(null);
 
   useEffect(() => {
     const element: HTMLElement | null = waypointRef.current;
-    const io = new IntersectionObserver((entries)=>{
-      console.log(entries[0].time);
+    const io = new IntersectionObserver((entries) => {
+      console.log(entries[0].isIntersecting ? 'entering' : 'leaving');
     });
     element && io.observe(element);
   }, [waypointRef]);
 
-  return (
+  return exposed ? (
+    children
+  ) : (
     <div ref={waypointRef} className='waypoint'>
       {children}
     </div>
